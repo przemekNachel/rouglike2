db72b1ceaa28568d7e178eccc3f1000a6b3d5c97
przemekNachel <przemeknachel@gmail.com>
2020-07-27 16:21:47 +0200

    Init commit

---
+++ Rougelike2-enter-colony/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rougelike2_enter_colony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
